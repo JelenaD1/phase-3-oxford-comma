0f12d91ab35c647ca131cda756a1f9486834dda2
Jelena <jelena.durovic@powerhrg.com>
2021-08-05 14:21:22 -0400

    commit tree 75ca351ecd842c5ea8caf56c53bebbc4eb8dab85

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
-def oxford_comma(array)
+# def oxford_comma(arr)
+#     if arr.length == 1
+#         arr.join
+#     elsif arr.length == 2
+#         arr.join(" and ")
+#     else
+#         # put a comma after each, but add "and" before the last word
+#         arr.each.collect do |value|
+#             if value == arr[-1]
+#                 arr[-1] = "and " << value
+#             end
+#         end
+#         arr.join(", ")
+#     end
+# end
 
-end
+
+#or
+def oxford_comma(array)
+case array.length 
+when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end 
